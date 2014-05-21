b6b8195d1a6b4a6c732d4c7d41e863e537c1fbd7
Eric Scharff <eric.scharff@gmail.com>
2014-05-21 11:21:29 -0400

    Provide package structure
    This moves objects into a Maven compatible format. It puts the source
    files into the standard hierarchy, and includes a pom.xml file so the
    binary can be built and run.

--- src/main/java/SmallObject.java
+++ src/main/java/SmallObject.java
@@ -6,6 +6,7 @@
 */
 
 import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
--- src/main/java/SmallWorld.java
+++ src/main/java/SmallWorld.java
@@ -9,6 +9,7 @@
 */
 
 import java.awt.GridLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
